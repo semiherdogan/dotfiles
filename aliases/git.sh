b40a10e0c0625bf62cfc6fd346565e551de610fb
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2020-12-28 17:12:47 +0300

    added new git aliases

--- aliases/git.sh
+++ aliases/git.sh
@@ -5,6 +5,13 @@
 alias g='git'
 alias nah='git reset --hard && git clean -df'
 
+alias add='git add .'
+alias pull='git pull'
+alias push='git push'
+alias status='git status'
+alias stash='git stash --include-untracked'
+alias stash-list='git stash list'
+alias pop='git stash pop'
 commit() {
     commitMessage="$1"
 
@@ -16,11 +23,9 @@ commit() {
     eval "git commit -a -m '${commitMessage}'"
 }
 
-alias pull='git pull'
-alias push='git push'
-alias status='git status'
-
 alias branch-authors='git for-each-ref --format="%(color:cyan)%(authordate:format:%Y-%m-%d %H:%M)   %(align:23,left)%(color:yellow)%(authorname)%(end) %(color:reset)%(refname:strip=3)" --sort=authordate refs/remotes'
+alias branch-name='git symbolic-ref --short HEAD'
+alias branch-rename='git branch -m'
 branch-create() {
     git fetch -pq && git checkout -b $2 --no-track origin/$1
 }
