b317377c0a6c1d73b921517b963c3f88abc24a10
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2022-02-18 09:32:30 +0300

    added laravel-new function

--- aliases/laravel.sh
+++ aliases/laravel.sh
@@ -83,3 +83,48 @@ laravel--delete-log-files () {
 
     cd $CURRENT_PATH
 }
+
+function laravel-new() {
+    local PROJECT_NAME="$1"
+
+    if [[ $PROJECT_NAME == "" ]]; then
+        echo "Provide a project name."
+
+        return
+    fi
+
+    docker info > /dev/null 2>&1
+
+    # Ensure that Docker is running...
+    if [ $? -ne 0 ]; then
+        echo "Docker is not running."
+
+        exit 1
+    fi
+
+    docker run --rm \
+        -v "$(pwd)":/opt \
+        -w /opt \
+        laravelsail/php81-composer:latest \
+        bash -c "laravel new $PROJECT_NAME && cd $PROJECT_NAME && php ./artisan sail:install --with=mysql,redis"
+
+    cd $PROJECT_NAME
+
+    CYAN='\033[0;36m'
+    LIGHT_CYAN='\033[1;36m'
+    WHITE='\033[1;37m'
+    NC='\033[0m'
+
+    echo ""
+
+    if sudo -n true 2>/dev/null; then
+        sudo chown -R $USER: .
+        echo -e "${WHITE}Get started with:${NC} cd $PROJECT_NAME && ./vendor/bin/sail up"
+    else
+        echo -e "${WHITE}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
+        echo ""
+        sudo chown -R $USER: .
+        echo ""
+        echo -e "${WHITE}Thank you! We hope you build something incredible. Dive in with:${NC} cd smh-app && ./vendor/bin/sail up"
+    fi
+}
